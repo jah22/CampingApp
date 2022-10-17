a7a25492d6c151fcdbf250d8d1c43b0597d1a939
lxaw <lawhalen@email.sc.edu>
2022-10-17 11:03:27 -0400

    quick fix for card

--- Gaurdian.java
+++ Gaurdian.java
@@ -2,16 +2,10 @@ import java.util.UUID;
 
 public class Gaurdian extends Person{
 
-    private Card card;
 
-    public Gaurdian(String firstName, String lastName,String birthDate, String address, UUID id, String password, String username, String email, String phone, Card card) {
+    public Gaurdian(String firstName, String lastName,String birthDate, String address, UUID id, String password, String username, String email, String phone) {
         super(firstName, lastName, birthDate, address, id);
         PriorityBehavior behavior = new PriorityBehavior(username,password,phone,email);
         this.setAuthBehavior(behavior);
-
-        this.card = card;
-    }
-    public boolean pay(double amount){
-        return this.card.charge(amount);
     }
 }

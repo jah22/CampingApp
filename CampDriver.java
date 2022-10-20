f9b19554879140e4272a8d181ff883dece9411d6
Jordan Fowler <jordanfowler2263@gmail.com>
2022-10-20 09:43:50 -0400

    Driver 2

--- CampDriver.java
+++ CampDriver.java
@@ -17,10 +17,100 @@ public class CampDriver {
                 System.out.println("Select:\n [1] to login to existing account \n [2] to register a new account");
                 String input = userInput.nextLine();
                 if(input.equals("1")) {
-                    
+                    System.out.println("Please enter your username.");
+                    String username = userInput.nextLine();
+
+                    System.out.print("Please enter your password.");
+                    String password = userInput.nextLine();
+
+                    //Implment to campsite manager login when ready
+                    System.out.println("You have sucesssfully logged in");
                 }
                 else if(input.equals("2")) {
+                    boolean registerQuit = true;
+                    while(registerQuit) { 
+                    System.out.println("Select: \n [1] Guardian \n [2] Coordinator \n [3] Administrator");
+                    String registerInput = userInput.nextLine();
+                        if(registerInput.equals("1")) {
+                            registerQuit = false;
+                            //Create a Guardian in Guardian Class
 
+                            //Ask for Guardian Information
+                            System.out.println("Please enter your first name");
+                            String userFirstName = userInput.nextLine();
+
+                            System.out.print("Please enter your last name");
+                            String userLastName = userInput.nextLine();
+
+                            System.out.println("Please enter your birthdate [input format]");
+                            String userBirthDate = userInput.nextLine();
+
+                            System.out.println("Please enter your home address");
+                            String userAddress = userInput.nextLine();
+
+                            System.out.println("Please enter your phone number");
+                            String phoneNumber = userInput.nextLine();
+
+                            System.out.println("Please enter your email address");
+                            String email = userInput.nextLine();
+
+                            //Give an ID
+
+                            System.out.println("Please enter a username you would like to use");
+                            String username = userInput.nextLine();
+
+                            System.out.println("Please enter a password you would like to use");
+                            String password = userInput.nextLine();
+
+                        }
+                        else if(registerInput.equals("2")) {
+                            registerQuit = false;
+                            //Create a Coordinator in Dependent Class
+
+                            //Ask for Coordinator Information
+                            System.out.println("Please enter your first name");
+                            String userFirstName = userInput.nextLine();
+
+                            System.out.print("Please enter your last name");
+                            String userLastName = userInput.nextLine();
+
+                            System.out.println("Please enter your birthdate [input format]");
+                            String userBirthDate = userInput.nextLine();
+
+                            System.out.println("Please enter your home address");
+                            String userAddress = userInput.nextLine();
+
+                            System.out.println("Please enter your phone number");
+                            String phoneNumber = userInput.nextLine();
+
+                            System.out.println("Please enter your email address");
+                            String email = userInput.nextLine();
+
+                            //Give an ID
+
+                            System.out.println("Please enter a username you would like to use");
+                            String username = userInput.nextLine();
+
+                            System.out.println("Please enter a password you would like to use");
+                            String password = userInput.nextLine();
+
+                            boolean ECBool = true;
+                            while(ECBool) {
+                                System.out.println("Do you want to add an Emergency Contact \n Select: [Y]es or [N]o");
+                                String EC = userInput.nextLine();
+                            }
+                            
+
+
+                        }
+                        else if(registerInput.equals("3")) {
+                            registerQuit = false;
+                            //Ask for Administrator Information
+                        }
+                        else {
+                            System.out.println("Try again");
+                        }
+                    }
                 }
                 else {
                     System.out.println("Try again");

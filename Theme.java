55fe2e08dce595d77b7558759a63c66123919891
Jacob Hammond <jacob.hammond18@gmail.com>
2022-11-01 17:49:25 -0400

    commented theme and theme Manager

--- Theme.java
+++ Theme.java
@@ -9,20 +9,38 @@ public class Theme {
     private int weekNumber;
     private String description;
 
+    /**
+     * Default constructor for Theme
+     * @param name, the name of the theme
+     * @param weekNumber, the week that the theme will be used at the camp
+     * @param description, the description of the theme
+     */
     public Theme(String name, int weekNumber, String description){
         this.name = name;
         this.weekNumber = weekNumber;
         this.description = description;
     }
+    /**
+     * @return the name of the theme
+     */
     public String getName(){
         return this.name;
     }
+    /**
+     * @return the number of the week 
+     */
     public int getWeekNumber(){
         return this.weekNumber;
     }
+    /**
+     * @return the description of the theme
+     */
     public String getDescription(){
         return this.description;
     }
+    /**
+     * @return a string representation of the Theme object
+     */
     public String toString(){
         return "Week: " + this.weekNumber + " -- Theme: " + this.name + "\nDescription: " + this.description;
     }
--- ThemeManager.java
+++ ThemeManager.java
@@ -10,18 +10,30 @@ import java.util.UUID;
 public class ThemeManager {
     private ArrayList<Theme> themes = new ArrayList<Theme>(); 
     private UUID id;
-
+    /**
+     * Empty constructor to set a random UUID
+     */
     public ThemeManager(){
-        // empty ctor
         this.id = UUID.randomUUID();
     }
+    /**
+     * Adds a Theme to an ArrayList of Theme objects
+     * @param theme, the Theme you wish to add to the ArrayList
+     */
     public void addTheme(Theme theme){
         this.themes.add(theme);
     }
+    /**
+     * Defualt constructor for ThemeManager
+     * @param themes, an ArrayList of Themes
+     */
     public void ThemeManager(ArrayList<Theme> themes){
         this.themes = themes;
         this.id = UUID.randomUUID();
     }
+    /**
+     * @return out, a string representation of the list of Themes
+     */
     public String toString(){
         String out = "";
         for(Theme theme:this.themes){
@@ -29,26 +41,42 @@ public class ThemeManager {
         }
         return out;
     }
+    /**
+     * @return the ID of the list of Themes for that camp
+     */
     public UUID getId(){
         return this.id;
     }
-
+    /**
+     * Sets the id for this list of Themes
+     */
     public void setId(UUID id){
         this.id = id;
     }
+    /**
+     * @return the length of the ArrayList of Themes
+     */
     public int getThemeCount(){
         return this.themes.size();
     }
+    /**
+     * @return the ArrayList of Themes
+     */
     public ArrayList<Theme> getThemes() {
         return this.themes;
     }
+    /**
+     * Prints out the list of Themes to the user
+     */
     public void viewThemes(){
         for(Theme theme: this.themes){
             System.out.println(theme);
         }
     }
+    /**
+     * Saves the list of Themes to the Theme.json file
+     */
     public void save(){
-        // save the themes
         ArrayList<ThemeManager> tmList = new ArrayList<ThemeManager>();
         tmList.add(this);
         FileIO.writeTheme(tmList);

56ef03b64cbf4eb43ed2f28bb4f2ea5448dc649d
lxaw <lawhalen@email.sc.edu>
2022-11-05 09:15:26 -0400

    add test classes

---
+++ testTheme.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import org.junit.Test;
+
+public class testTheme{
+
+    @BeforeClass
+    public static void oneTimeSetup()[
+
+    ]
+    @AfterClass
+    public static void oneTimeTearDown(){
+
+    }
+    @BeforeEach
+    public static void setup(){
+        // runs before each test
+    }
+    @AfterEach
+    public static void tearDown(){
+        // runs after each test
+    }
+}
